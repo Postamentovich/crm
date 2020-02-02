3a2dbe07ea2dba911c9d848744aba9062b8e82db
postamentovich <postamentovich@gmail.com>
2020-02-02 14:36:33 +0300

    add route auth

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -2,7 +2,8 @@ const messages = {
   logout: "Вы вышли из системы",
   "auth/user-not-found": "Такого пользователя не существует",
   "auth/wrong-password": "Неверный пароль ил email",
-  "auth/email-already-in-use": "Пользователь уже существует"
+  "auth/email-already-in-use": "Пользователь уже существует",
+  login: "Авторизуйтесь"
 };
 
 export default messages;
